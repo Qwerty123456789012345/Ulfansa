52e9d88d21c2f04812b49c8a4096daea88cc59da
sahilbest999 <sahilpshahane123@gmail.com>
2020-05-31 23:24:13 +0530

    Made Many changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(".hamburger").click(function(){
 });
 
 $(".post-image").slideDown("slow");
-
 if(testing==false)
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -12,3 +11,42 @@ firebase.auth().onAuthStateChanged(function(user) {
         location.replace("https://ulfansa-1590486246579.web.app/login.html");
     }
   });
+
+var inputs = $("#h-menu input[type=radio]");
+
+$(document).ready(function () {
+
+$("#h-menu input[type=radio]").on("change",function(e){
+
+  if($("#home-btn").prop("checked")==false){
+    $("#home").css("display","none");
+  }
+  else{
+    $("#home").css("display","block");
+  }
+
+  if($("#gallery-btn").prop("checked")==false){
+    $("#gallery").css("display","none");
+  }
+  else{
+    $("#gallery").css("display","block");
+  }
+
+  if($("#user-setting-btn").prop("checked")==false){
+    $("#user-setting").css("display","none");
+  }
+  else{
+    $("#user-setting").css("display","block");
+  }
+});
+
+
+$("#logout-btn").click(function(){
+  firebase.auth().signOut().then(function() {
+ 
+  }).catch(function(error) {
+    alert("Something Went Wrong!");
+  });
+});
+});
+
